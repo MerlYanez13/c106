48f5f61d806045d4c948b969de05131ddb53d572
MerlYanez13 <71855710+MerlYanez13@users.noreply.github.com>
2021-10-20 20:57:45 -0400

    Add files via upload

--- coffee.py
+++ coffee.py
@@ -1,6 +1,23 @@
 import plotly.express as px
 import csv
-with open ("Coffee.csv") as f:
-    df=csv.DictReader(f)
-    fig=px.scatter(df,x="Coffee in ml", y="sleep in hours", color="week")
-    fig.show()
+import numpy as np
+
+def getDataSource(path):
+    Coffee=[]
+    Sleep=[]
+    with open (path) as f:
+        df=csv.DictReader(f)
+        for row in df:
+            Coffee.append(float(row["Coffee in ml"]))
+            Sleep.append (float(row["sleep in hours"]))
+        #fig=px.scatter(df,y="Coffee In Percentage", x="Sleep Present")
+        #fig.show()
+    return {"x":Coffee,"y":Sleep}
+
+def findCorrelation(ds):
+    correlation=np.corrcoef(ds["x"],ds["y"])
+    print("correlation= ",correlation[0,1])
+
+dataPath="Coffee.csv"
+ds=getDataSource(dataPath)
+findCorrelation(ds)
